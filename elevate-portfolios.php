fade26eff06b2a14329615d40c1bd87367cd644f
Simon Codrington <simonrcodrington@gmail.com>
2016-12-07 11:38:15 +1100

    Fixed get_portfolio_for_term argument error

--- elevate-portfolios.php
+++ elevate-portfolios.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://elevate360.com.au/plugins
  * Description: Showcases portfolios with an easy to use admin back-end. Contains a filterable listing page for portfolios plus a single portfolio showcase. Use a combination of
  * either shortcodes or action hooks to output content for your single portfolio pages. All portfolios are enriched with schema.org metadata  
- * Version:     1.1.9
+ * Version:     1.1.10
  * Author:      Simon Codrington
  * Author URI:  https://simoncodrington.com.au
  * Text Domain: elevate-portfolios
@@ -1242,7 +1242,7 @@
 			$term_args = array(
 				'post_type'		=> $instance->post_type_args['post_type_name'],
 				'post_status'	=> 'publish',
-				'posts_par_page'=> -1,
+				'posts_per_page'=> -1,
 				'tax_query'		=> array(
 					array(
 						'taxonomy'	=> $term->taxonomy,
